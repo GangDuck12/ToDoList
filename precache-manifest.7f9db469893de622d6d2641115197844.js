c853a681ef43080b56e8417b559eeebf51ff5194
gangduck <GangDuck12@gmail.com>
2020-07-09 21:51:11 +0900

    Updates

---
+++ precache-manifest.7f9db469893de622d6d2641115197844.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c509abdd7dc67f146e8d8a7d52ab6fa",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "ebd13d82f132f4976b91",
+    "url": "/ToDoList/static/css/main.3df0526c.chunk.css"
+  },
+  {
+    "revision": "eff5878e25b77860409e",
+    "url": "/ToDoList/static/js/2.67409984.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoList/static/js/2.67409984.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebd13d82f132f4976b91",
+    "url": "/ToDoList/static/js/main.ea146c00.chunk.js"
+  },
+  {
+    "revision": "a8361f3a72cc3929a621",
+    "url": "/ToDoList/static/js/runtime-main.3c62dd5d.js"
+  }
+]);
